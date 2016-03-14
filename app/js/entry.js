e839d4f3dd05718ac4ffa48294a1027c4fa11d34
DAVID PARK <DPARK4657@GMAIL.COM>
2016-03-13 20:48:46 -0700

    fixed conflicts

---
+++ app/js/entry.js
@@ -0,0 +1,13 @@
+require('angular/angular');
+require('angular-route');
+var angular = window.angular;
+
+var grokApp = angular.module('GrokApp', ['ngRoute']);
+
+
+
+// 
+// grokApp.config(['$routeProvider', function($route) {
+//   $route
+//     .when()
+// }])
--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,36 @@ const eslint = require('gulp-eslint');
 const mocha = require('gulp-mocha');
 const webpack = require('webpack-stream');
 const Server = require('karma').Server;
+var concatCss = require('gulp-concat-css');
+
 
 const allScripts = ['**/*.js', '!node_modules/**'];
 const serverTestFiles = ['models/*.js', 'routes/*.js'];
+
+
+gulp.task('static:dev', () => {
+  gulp.src('app/**/*.html')
+  .pipe(gulp.dest('build/'));
+});
+
+gulp.task('webpack:dev', () => {
+  return gulp.src('app/js/entry.js')
+  .pipe(webpack({
+    output: {
+      filename: 'bundle.js'
+    }
+  }))
+  .pipe(gulp.dest('build/'));
+});
+
+
+gulp.task('webpack:css', function () {
+  return gulp.src('app/sass/*.css')
+    .pipe(concatCss("bundle.css"))
+    .pipe(gulp.dest('build/'));
+});
+
+
 
 gulp.task('lint', () => {
   return gulp.src(allScripts)
@@ -32,8 +59,19 @@ gulp.task('test:server', () => {
     .pipe(mocha());
 });
 
+
 gulp.task('watch', () => {
   gulp.watch(allScripts, ['lint']);
 });
 
 gulp.task('default', ['watch']);
+
+gulp.task('watch', function() {
+    gulp.watch('app/js/*.js', ['webpack:dev'] );
+    gulp.watch('app/sass/*.css', ['webpack:css'] );
+    gulp.watch('app/**/*.html', ['static:dev']);
+});
+
+gulp.task('build', ['webpack:dev', 'static:dev', 'webpack:css']);
+gulp.task('default', ['lint', 'mocha']);
+
