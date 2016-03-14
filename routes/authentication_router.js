6736d349a1e8a373a787a5f39af4f5a1e4870ea8
DAVID PARK <DPARK4657@GMAIL.COM>
2016-03-13 21:08:50 -0700

    server.js, gulpfilejs, lint errors cleanup

--- routes/authentication_router.js
+++
@@ -1,5 +0,0 @@
-const express = require('express');
-const jsonParser = require('body-parser').json();
-const User = require(__dirname + '/../models/reviewUser');
-const handleDBError = require(__dirname + '/../lib/handle_db_error');
-
--- server.js
+++ server.js
@@ -3,20 +3,13 @@ const app = express();
 const mongoose = require('mongoose');
 const businessRouter = require(__dirname + '/routes/business_router');
 const reviewRouter = require(__dirname + '/routes/reviews_router');
-// const Authenticat = new Authenticat('authenticat');
-// const authenticat = new Authenticat(connection);
 
 mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/grok_dev');
 
 app.use(express.static(__dirname + '/build'));
 
-// app.use('/api', authenticat.router);
 app.use('/api', businessRouter);
 app.use('/api', reviewRouter);
-
-// app.get('/user', authenticat.tokenAuth, (req,res) => {
-//   return res.json({username: req.user.username});
-// });
 
 var PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log('Server started on port: ' + PORT));
