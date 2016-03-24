b06c93d4176a388b965c28c0eda3f5aaa0510041
DanLombardy <lombardy.dan@gmail.com>
2016-03-24 15:47:28 -0700

    Fix cloud-data errors

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
+
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const businessRouter = require(__dirname + '/routes/business_router');
 const reviewRouter = require(__dirname + '/routes/reviews_router');
+const jsonParser = require('body-parser').json();
+const cloudTokenizer = require(__dirname + '/lib/cloud-tokenizer.js');
 
 mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/grok_dev');
 
@@ -11,7 +14,14 @@ app.use(express.static(__dirname + '/build'));
 app.use('/api', businessRouter);
 app.use('/api', reviewRouter);
 
+app.post('/cloudData', jsonParser, (req, res)=>{
+  var obj = req.body;
+  console.log(req.body);
+  var tokenCloud = cloudTokenizer(obj);
+  console.log(tokenCloud);
+  res.json(tokenCloud);
+  res.end();
+})
+
 var PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log('Server started on port: ' + PORT));
-
-
