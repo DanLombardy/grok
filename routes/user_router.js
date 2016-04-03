eb6b123fc1eecc1c1eb74dac92cb3e80158f8d77
Austin King <aust.a.king@gmail.com>
2016-04-02 20:55:37 -0700

    Added basic server-side auth.

---
+++ routes/user_router.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const jsonParser = require('body-parser').json();
+const httpBasic = require(__dirname + '/../lib/http_basic');
+const BusinessUser = require(__dirname + '/../models/business_user');
+const PublicUser = require(__dirname + '/../models/public_user');
+const handleError = require(__dirname + '/../lib/handle_error');
+const userEvents = require(__dirname + '/user_events');
+
+const userRouter = module.exports = exports = express.Router();
+
+userRouter.post('/signup', jsonParser, (req, res) => {
+  userEvents.emit('create_new_user', req, res);
+});
+
+userRouter.get('/signin', httpBasic, (req, res) => {
+  userEvents.emit('log_in_user', req, res);
+});
--- server.js
+++ server.js
@@ -2,14 +2,18 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const businessRouter = require(__dirname + '/routes/business_router');
-const reviewRouter = require(__dirname + '/routes/reviews_router');
+const reviewRouter = require(__dirname + '/routes/review_router');
+const userRouter = require(__dirname + '/routes/user_router');
 
 mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/grok_dev');
+
+process.env.APP_SECRET = process.env.APP_SECRET || 'UNSECURECHANGEMECHANGEME';
 
 app.use(express.static(__dirname + '/build'));
 
 app.use('/api', businessRouter);
 app.use('/api', reviewRouter);
+app.use('/api', userRouter);
 
 var PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log('Server started on port: ' + PORT));
